d022f9efa45210dd448f1406e6e89df0f0443084
manas <manasrai1234567@gmail.com>
2023-02-25 18:57:00 +0530

    Commit 25/2

--- app/src/main/java/com/me_manas/quickgpt/MainActivity.kt
+++ app/src/main/java/com/me_manas/quickgpt/MainActivity.kt
@@ -8,6 +8,13 @@ import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import okhttp3.*
+
+import okhttp3.RequestBody.Companion.toRequestBody
+
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,18 +24,20 @@ class MainActivity : AppCompatActivity() {
     lateinit var send_button : ImageButton
     lateinit var messageList : ArrayList<Message_Chat>
     lateinit var Message_aAdapter : MessageAdapter
+    val JSON = "application/json; charset=utf-8".toMediaTypeOrNull()
+    val client = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        messageList  = ArrayList()
         recycler_view = findViewById(R.id.recycler_view)
         start_message = findViewById(R.id.start_chat)
         enter_message = findViewById(R.id.enter_text)
         send_button = findViewById(R.id.sendButton)
 
          // set up recycler view
-        messageList = ArrayList()
         Message_aAdapter = MessageAdapter(messageList)
         recycler_view.adapter = Message_aAdapter
         recycler_view.layoutManager = LinearLayoutManager(this)
@@ -37,6 +46,7 @@ class MainActivity : AppCompatActivity() {
         send_button.setOnClickListener {
             val text = enter_message.text.toString().trim()
             addToMessageList(text,"me")
+            apiCall(text)
             enter_message.setText("")
             start_message.visibility = View.GONE
         }
@@ -49,5 +59,55 @@ class MainActivity : AppCompatActivity() {
             recycler_view.smoothScrollToPosition(Message_aAdapter.itemCount)
         })
 
+    }
+
+    fun addToMessageResponse(repsonse: String){
+        addToMessageList(repsonse,"bot")
+    }
+
+    fun apiCall(question:String){
+
+        val jsonBody = JSONObject()
+        try {
+            jsonBody.put("model", "text-davinci-003")
+            jsonBody.put("prompt", question)
+            jsonBody.put("max_tokens", "4000")
+            jsonBody.put("temperature", 0.6)
+        }catch (e: java.lang.Exception){
+            e.printStackTrace()
+        }
+
+        val body = jsonBody.toString().toRequestBody(JSON)
+
+        val headers = Headers.Builder()
+            .add("Authorization", "Bearer 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")
+        val request : Request = Request.Builder()
+            .url("https://api.openai.com/v1/completions")
+            .headers(headers.build())
+            .post(body)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback{
+            override fun onFailure(call: Call, e: IOException) {
+                // Handle the failure
+                addToMessageResponse("Failed to load response due to(onFailure) "+e.message)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                // Handle the response
+                if(response.isSuccessful){
+                    try {
+                        val jsonObject = JSONObject(response.body.toString())  // .string()
+                        val jsonArray = jsonObject.getJSONArray("choices")
+                        val result = jsonArray.getJSONObject(0).getString("text")
+                        addToMessageResponse(result.trim())
+                    }catch (e : java.lang.Exception) {
+                        e.printStackTrace()
+                    }
+                } else{
+                    addToMessageResponse("Failed to load response due to "+response.body.toString())
+                }
+            }
+        })
     }
 }
